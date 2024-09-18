4d0b75e97a1a5b1efdc8b0ef8eb67a0123006487
Kaspar Yorrick Laaser <kaspar.laaser@rwth-aachen.de>
2024-09-18 19:04:31 +0200

    remove old game loop

--- src/main.rs
+++ src/main.rs
@@ -97,68 +97,6 @@ fn struct_game_loop() {
     }
 }
 
-pub fn old_game_loop() {
-    let secret = Player::Keeper.read_input();
-
-    if !secret.chars().all(|c| c.is_digit(10)) {
-        println!("The secret must be composed of digits only!");
-        return;
-    }
-
-    let mut game = Game::new(secret);
-
-    main_game_loop(&mut game);
-}
-
-fn main_game_loop(game: &mut Game) {
-    loop {
-        let player = Player::Seeker;
-        player.display_guesses(game.get_previous_guesses());
-
-        let guess = player.read_input();
-        match game.handle_guess(&guess) {
-            Ok(score) => {
-                player.display_message(&score.display());
-                if score.bulls == game.get_secret_len() {
-                    player.display_message("Congratulations! You've guessed the secret.");
-                    player.display_guesses_colorified(
-                        game.get_previous_guesses(),
-                        &game.get_secret(),
-                    );
-                    break;
-                }
-            }
-            Err(msg) => {
-                player.display_message(msg);
-                continue;
-            }
-        }
-
-        let player = Player::Keeper;
-
-        player.display_guesses_colorified(game.get_previous_guesses(), &game.get_secret());
-        // Handle secret change...
-        'fetch_new_secret: loop {
-            let new_secret = player.read_input();
-
-            let result = game.change_secret(&new_secret);
-            match result {
-                Ok(()) => break 'fetch_new_secret,
-                Err(response) => {
-                    player.display_message(&response.message());
-                    match response {
-                        ErrResponse::GuessMismatch(guesses) => {
-                            player.display_guesses_colorified(&guesses, &new_secret)
-                        }
-                        _ => (),
-                    }
-                    continue 'fetch_new_secret;
-                }
-            }
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use colored::Colorize;
