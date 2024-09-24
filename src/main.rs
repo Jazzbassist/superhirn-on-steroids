474a65071e05f8c6a3e63688381eea0f7a4ebea4
Kaspar Yorrick Laaser <kaspar.laaser@rwth-aachen.de>
2024-09-24 22:34:03 +0200

    gameloop to own module

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,10 @@
 // main.rs
 mod game;
+mod gameloop;
 mod ui;
 
 use game::*;
-use ui::*;
-
-struct GameLoop {
-    pub game: Game,
-    pub player: Player,
-    pub is_over: bool,
-}
-
-impl GameLoop {
-    pub fn new(variant: Variant) -> GameLoop {
-        GameLoop {
-            game: Game::new(variant),
-            player: Player::Keeper,
-            is_over: false,
-        }
-    }
-
-    fn switch_player(&mut self) {
-        match self.player {
-            Player::Keeper => self.player = Player::Seeker,
-            Player::Seeker => self.player = Player::Keeper,
-        }
-        self.print_state();
-    }
-
-    pub fn prompt_input(&mut self) -> String {
-        self.player.read_input()
-    }
-
-    fn print_state(&self) {
-        self.player
-            .display_guesses(self.game.get_previous_guesses());
-    }
-
-    pub fn take_input(&mut self, input: &str) {
-        match self.player {
-            Player::Keeper => self.attempt_change_secret(input),
-            Player::Seeker => self.attempt_guess(input),
-        }
-    }
-
-    fn attempt_change_secret(&mut self, new_secret: &str) {
-        let result = self.game.change_secret(new_secret);
-        match result {
-            Ok(()) => self.switch_player(),
-            Err(response) => {
-                self.player.display_message(&response.message());
-                match response {
-                    ErrResponse::GuessMismatch(guesses) => self
-                        .player
-                        .display_guesses_colorified(&guesses, &new_secret),
-                    _ => (),
-                }
-            }
-        }
-    }
-
-    fn attempt_guess(&mut self, new_guess: &str) {
-        let result = self.game.handle_guess(new_guess);
-        match result {
-            Ok(score) => {
-                self.player.display_message(&score.display());
-                if score.bulls == self.game.get_secret_len() {
-                    self.player
-                        .display_message("Congratulations! You've guessed the secret.");
-                    self.player.display_guesses_colorified(
-                        self.game.get_previous_guesses(),
-                        &self.game.get_secret(),
-                    );
-                    self.is_over = true;
-                }
-                self.switch_player();
-            }
-            Err(msg) => {
-                self.player.display_message(msg);
-            }
-        }
-    }
-}
+use gameloop::*;
 
 fn main() {
     struct_game_loop()
