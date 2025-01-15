aca333156513e49ce6e1c54130f7892b249ece8e
Kaspar Yorrick Laaser <kaspar.laaser@rwth-aachen.de>
2025-01-16 00:05:48 +0100

    make buildable, add wasm_bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-pub fn init(variant: String) {
+use wasm_bindgen::prelude::wasm_bindgen;
+mod game;
+mod gameloop;
+mod ui;
+
+use gameloop::*;
+
+#[wasm_bindgen]
+pub fn init(_variant: String) {
     //let mut gameloop = GameLoop::new(Variant::Curtail);
     let mut gameloop = GameLoop::new(Variant::ChangeSecret);
     gameloop.run();
+}
+
+#[wasm_bindgen]
+pub fn add (a: usize, b: usize) -> usize {
+    a+b
 }
