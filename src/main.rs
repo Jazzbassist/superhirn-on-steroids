e0e72decd34d4fe3e7c379c1459d9eae3e068c97
Kaspar Yorrick Laaser <kaspar.laaser@rwth-aachen.de>
2025-01-15 23:09:20 +0100

    add some commented lines to remember what we lost

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use game::*;
+
 fn main() {
+    //let mut gameloop = GameLoop::new(Variant::Curtail);
+    //let mut gameloop = Game::new(Variant::ChangeSecret);
+    //gameloop.run();
     println!("Hello, world!");
 }
